0e1e79db814cbd6e5be7d9fba4182e9ccb07960b
aaronkoichi <aaronlee4399@gmail.com>
2024-11-23 23:15:41 +0800

    New config based off nvchad

---
+++ lua/mappings.lua
@@ -0,0 +1,44 @@
+local map = vim.keymap.set
+
+-- general mappings
+map("n", "<C-s>", "<cmd> w <CR>")
+map("i", "jk", "<ESC>")
+map("n", "<C-c>", "<cmd> %y+ <CR>") -- copy whole filecontent
+
+-- nvimtree
+map("n", "<C-n>", "<cmd> NvimTreeToggle <CR>")
+map("n", "<C-h>", "<cmd> NvimTreeFocus <CR>")
+
+-- telescope
+map("n", "<leader>ff", "<cmd> Telescope find_files <CR>")
+map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>")
+map("n", "<leader>fw", "<cmd> Telescope live_grep <CR>")
+map("n", "<leader>gt", "<cmd> Telescope git_status <CR>")
+
+-- bufferline, cycle buffers
+map("n", "<Tab>", "<cmd> BufferLineCycleNext <CR>")
+map("n", "<S-Tab>", "<cmd> BufferLineCyclePrev <CR>")
+map("n", "<C-q>", "<cmd> bd <CR>")
+
+-- comment.nvim
+map("n", "<leader>/", "gcc", { remap = true })
+map("v", "<leader>/", "gc", { remap = true })
+
+-- format
+map("n", "<leader>fm", function()
+  require("conform").format()
+end)
+
+-- move through visual mode
+map("v", "<S-down>", ":m '>+1<CR>gv=gv")
+map("v", "<S-up>", ":m '<-2<CR>gv=gv")
+
+-- half page jump
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- navigation
+map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
---
+++ lua/options.lua
@@ -0,0 +1,35 @@
+local o = vim.o
+
+vim.g.mapleader = " "
+
+o.laststatus = 3 -- global statusline
+o.showmode = false
+
+o.clipboard = "unnamedplus"
+
+-- Indenting
+o.expandtab = true
+o.smartindent = true
+o.tabstop = 4
+o.shiftwidth = 4
+vim.opt.fillchars = { eob = " " }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+
+o.number = true
+
+o.scrolloff = 8
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.termguicolors = true
+o.timeoutlen = 400
+o.undofile = true
+o.cursorline = true
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
+
+vim.api.nvim_set_hl(0, "IndentLine", { link = "Comment" })
